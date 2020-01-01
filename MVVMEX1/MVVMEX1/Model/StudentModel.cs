46cd436ad1536d74bb23b1f753438fb8b171c027
dkxltks25 <dkxltks25@naver.com>
2020-01-01 17:19:47 +0900

    Happy New Year

---
+++ MVVMEX1/MVVMEX1/Model/StudentModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel;
+namespace MVVMEX1.Model
+{
+    public class StudentModel {}
+    public class Student : INotifyPropertyChanged
+    {
+        private string firstName;
+        private string lastName;
+        
+        public string FirstName
+        {
+            get { return firstName; }
+            set
+            {
+                firstName = value;
+                RaisePropertyChanged("FirstName");
+                RaisePropertyChanged("FullName");
+            }
+        }
+        public string LastName
+        {
+            get { return lastName; }
+            set
+            {
+                lastName = value;
+                RaisePropertyChanged("LastName");
+                RaisePropertyChanged("FullName");
+            }
+        }
+        public string FullName
+        {
+            get { return FirstName + " " + LastName; }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged(string property)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+    }
+    
+}
---
+++ MVVMEX1/MVVMEX1/ViewModel/StudentViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MVVMEX1.Model;
+using System.Collections.ObjectModel;
+namespace MVVMEX1.ViewModel
+{
+    public class StudentViewModel
+    {
+        public StudentViewModel()
+        {
+            LoadStudents();
+        }
+        public ObservableCollection<Student> Students
+        {
+            get;
+            set;
+        }
+        public void LoadStudents()
+        {
+            ObservableCollection<Student> students = new ObservableCollection<Student>();
+            students.Add(new Student { FirstName = "Mark", LastName = "Allain" });
+            students.Add(new Student { FirstName = "Allen", LastName = "Brown" });
+            students.Add(new Student { FirstName = "Linda", LastName = "Hamerski" });
+            for(int i = 0; i < students.Count; i++)
+            {
+                Console.WriteLine(students[i].ToString());
+            }
+            Students = students;
+        }
+    }
+}
